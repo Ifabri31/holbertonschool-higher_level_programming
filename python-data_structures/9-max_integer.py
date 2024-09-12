6c7006f5dd7a1effb109456add741a3b0fa7943c
Fabri <oviedofarbizzio1998@gmail.com>
2024-09-12 09:43:57 -0300

    Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    num_max = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > num_max:
+            num_max = my_list[i]
+    return num_max
     
