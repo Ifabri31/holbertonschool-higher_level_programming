fcb45cb466af63071b90e8eeeb0f44809c0ce3b4
Fabri <oviedofabrizzio1998@gmail.com>
2024-09-17 16:09:12 -0300

    fix documentacion

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square which represents a square.
+
+It contains methods to set and retrieve the size and position of the square,
+compute its area, and print the square with '#' characters.
+"""
+
+
 class Square:
+    """
+    The class 'Square' represent a square
+    """
     pass
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square which represents a square.
+
+It contains methods to set and retrieve the size and position of the square,
+compute its area, and print the square with '#' characters.
+"""
+
+
 class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
