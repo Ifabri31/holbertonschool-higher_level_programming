489d2ff3c7b4d647480963f431b69d3567f345a0
Fabri <oviedofabrizzio1998@gmail.com>
2024-09-17 15:32:57 -0300

    fix 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square which represents a square.
+
+It contains methods to set and retrieve the size and position of the square,
+compute its area, and print the square with '#' characters.
+"""
+
 class Square:
     """
     Class that defines a square with size and position attributes.
