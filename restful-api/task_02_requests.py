8d3546df5cf77d6d789735c57a71abc48ca25da3
Fabri <oviedofabrizzio1998@gmail.com>
2024-10-14 14:52:40 -0300

    task 02

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    solicitud = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        posts = solicitud.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    solicitud = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    if solicitud.status_code == 200:
+        response_json = solicitud.json()
+
+        with open('posts.csv', mode='w', encoding='utf-8', newline='') as i:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(i, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            for post in response_json:
+                writer.writerow({'id': post['id'], 'title': post['title'], 'body': post['body']})
