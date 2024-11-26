ca8835417ed92eb0ad607d1315ea17790dfc8e70
Fabri <oviedofabrizzio1998@gmail.com>
2024-11-26 20:58:24 -0300

    fix style task4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """
 This module defines a Rectangle class that represents a rectangle
 with width and height attributes, along with methods to get and set them.
 """
+
 
 class Rectangle:
     """
