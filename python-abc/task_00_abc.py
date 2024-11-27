d5eb34c3bcd7842415e22518e3f318acf680e9be
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2024-11-26 21:45:14 -0300

    task00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+
+#if __name__ == "__main__":
+    #bobby = Dog()
+    #garfield = Cat()
+    
+    #print(bobby.sound())
+    #print(garfield.sound(
+
+
+    #animal = Animal()
