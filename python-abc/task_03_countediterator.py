ffeacd06ff3957e51cbf95eb713d2ce5c5950d3b
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2024-11-26 21:55:16 -0300

    borrando lineas no son necesariaas

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,14 +9,3 @@ class CountedIterator:
     
     def get_count(self):
         return self.count
-
-if __name__ == "__main__":
-    data = [1, 2, 3, 4]
-    counted_iter = CountedIterator(data)
-
-    try:
-        while True:
-            item = next(counted_iter)
-            print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
-    except StopIteration:
-        print("No more items.")
