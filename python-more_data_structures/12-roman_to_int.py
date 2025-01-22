dd6c40f6d5d2d390698db0f82395b848b59d89e5
Fabrizzio <oviedofabrizzio1998@gmail.com>
2025-01-22 14:15:49 -0300

    fix task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    for i in roman_string:
-        if i == '[0-9]' or None:
-            return 0
+    if type(roman_string) is str or roman_string is None:
+        return 0
     
     dict_roman = {'I = 1' , 'V = 5' , 'X = 10' , 'L = 50' , 'C = 100' , 'D = 500' , 'M = 1000'}
     total = 0
