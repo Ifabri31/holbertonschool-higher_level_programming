175c3d308a3853b59a5f2238db9d39ed82fb397c
Fabrizzio <oviedofabrizzio1998@gmail.com>
2025-01-22 14:32:10 -0300

    fix validator is not a str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is str or roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
     
-    dict_roman = {'I = 1' , 'V = 5' , 'X = 10' , 'L = 50' , 'C = 100' , 'D = 500' , 'M = 1000'}
+    dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C' : 100, 'D' : 500, 'M' : 1000}
     total = 0
     
     for x in range(len(roman_string)):
