a2dc22e523057fa6ee2cbe5c7dc4cac279141085
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-01-30 12:37:00 -0300

    fix task 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -48,7 +48,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         for row in range(self.height):
-            print("#" * self.width)
+            return("#" * self.width)
 
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
