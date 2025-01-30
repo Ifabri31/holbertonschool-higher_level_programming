8161382b91289f4e1cf1dddf6dfadcf4e6a542af
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-01-30 12:09:09 -0300

    1.1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     Class documented
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
+        self.width = width
         self.height = height
 
     @property
