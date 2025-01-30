9be4836879097b1ac3647c4c0c175340cc067dfd
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-01-30 11:55:30 -0300

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,4 +8,16 @@ class Rectangle:
     """
     Class documented
     """
-    pass
+    def __init__(self, width=0, height=0):
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value)is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("heigth must be >= 0")
