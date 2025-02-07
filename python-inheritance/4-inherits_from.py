4289c799d2a7262b9459f542784c6bb89c28030e
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-02-07 17:19:48 -0300

    docs: improve docstring formatting in inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,8 @@ Module documented: 4-inherits_from
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance of a class that inherited from a class"""
+    """
+    Check if an object is an instance of a
+    class that inherited from a class
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
