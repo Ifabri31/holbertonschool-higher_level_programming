44c219135f1be583e15ba62017fa577969309bbf
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-02-07 17:19:15 -0300

    fix: correct type comparison in inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ Module documented: 4-inherits_from
 
 def inherits_from(obj, a_class):
     """Check if an object is an instance of a class that inherited from a class"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
