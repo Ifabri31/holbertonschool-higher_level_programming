b1fb5a7615c998d8785f319993bf5fefd7b13df9
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-02-10 16:30:42 -0300

    implement write_file function to write a string to a text file and return the number of characters written

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """
+    Write a string to a text file (UTF8) and
+    returns the number of characters written
+    """
+    with open(filename, 'w') as file:
+        return file.write(text)
+    
