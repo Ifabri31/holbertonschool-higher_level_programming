414a6fe4f40638b63978ec761e0b39dc3aaa1706
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-02-10 16:22:34 -0300

    update .gitignore to exclude .txt files; improve docstring format in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    """
     with open(filename, 'r') as file:
         content = file.read()
         print(content)
