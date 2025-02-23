a180069a0801d7d408e2f729b23c9e223a440c6f
Fabrizzio Oviedo <oviedofabrizzio1998@gmail.com>
2025-02-23 15:23:03 -0300

    feat: add initial implementation of RESTful API with post fetching and CSV saving functionality

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,24 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
+    print("Status code:", response.status_code)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        with open('posts.csv', mode='w', encoding='utf-8', newline='') as f:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()         
+            for post in posts:
+                writer.writerow({'id': post['id'], 'title': post['title'], 'body': post['body']})
